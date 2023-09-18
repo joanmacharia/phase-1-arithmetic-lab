fe17aca8883b9b1ca0bfc85f99e80378bfe2c3bd
Joan Macharia <joan.macharia@students.moringa>
2023-09-18 22:02:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+console.log(multiply)
+
+const random = Math.floor(Math.random() * 10) + 1
+
+const num3 = 16
+const num4 = 6
+const mod = 16 % 6
+console.log (mod)
+
+const setOfNumbers = [5,10,15,20,16]
+const max = Math.max(...setOfNumbers)
+console.log(max)
